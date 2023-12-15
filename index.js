2fe1411fd26c60bd6c1ddec5d53409f36fa2685e
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-15 12:31:03 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+// 1. companyName is set as Scuber
+const companyName = 'Scuber';
+
+// 2. mostProfitableNeighborhood is declared as equal to Chelsea
+let mostProfitableNeighborhood = 'Chelsea';
+
+// 3. companyCeo is declared as equal to Susan Smith
+let companyCeo = 'Susan Smith';
+
+
